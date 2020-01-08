920862e97b13f09750a0d86e9be1f9a55f18c7d4
Chenstyle <lizhizhao81@gmail.com>
2020-01-09 01:43:35 +0800

    使用ViewPager搭建图片显示架构

---
+++ app/src/main/java/com/example/wechatframe/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.wechatframe;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.wechatframe.fragment.SplashFragment;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ViewPager mVpSplash;
+
+    private int[] mResIds = new int[] {
+            R.drawable.guide_image1,
+            R.drawable.guide_image2,
+            R.drawable.guide_image3,
+            R.drawable.guide_image4
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mVpSplash = findViewById(R.id.vp_splash);
+        mVpSplash.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return SplashFragment.newInstance(mResIds[position]);
+            }
+
+            @Override
+            public int getCount() {
+                return mResIds.length;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/wechatframe/fragment/SplashFragment.java
@@ -0,0 +1,50 @@
+package com.example.wechatframe.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.wechatframe.R;
+
+public class SplashFragment extends Fragment {
+    private static final String BUNDLE_KEY_RES_ID = "bundle_key_res_id";
+
+    private ImageView mIvContent;
+    private int mResId;
+
+    public static SplashFragment newInstance(int resId) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(BUNDLE_KEY_RES_ID, resId);
+        SplashFragment splashFragment = new SplashFragment();
+        splashFragment.setArguments(bundle);
+        return splashFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            mResId = arguments.getInt(BUNDLE_KEY_RES_ID);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mIvContent = view.findViewById(R.id.iv_content);
+        mIvContent.setImageResource(mResId);
+    }
+}
