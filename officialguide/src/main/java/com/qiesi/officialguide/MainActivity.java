69f181c187d7f738d124ba1c3f2c7326daf3ebfe
Chenstyle <lizhizhao81@gmail.com>
2020-01-15 17:18:17 +0800

    motion

--- officialguide/src/main/java/com/qiesi/officialguide/MainActivity.java
+++ officialguide/src/main/java/com/qiesi/officialguide/MainActivity.java
@@ -1,8 +1,12 @@
 package com.qiesi.officialguide;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.qiesi.officialguide.motionlayout.MotionLayoutActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void motionLayout(View view) {
+        startOther(MotionLayoutActivity.class);
+    }
+
+    private void startOther(Class activityClass) {
+        startActivity(new Intent(MainActivity.this, activityClass));
+    }
+
 }
---
+++ officialguide/src/main/java/com/qiesi/officialguide/motionlayout/MotionLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.qiesi.officialguide.motionlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.qiesi.officialguide.R;
+
+public class MotionLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_layout);
+    }
+}
